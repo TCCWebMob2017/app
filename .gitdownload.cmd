f270441825b47b028c468b3a7cc39ed64f06176a
Alcenir Felix <felix.alcenir@gmail.com>
2019-02-08 16:21:52 -0200

    Initial commit

---
+++ .gitdownload.cmd
@@ -0,0 +1,7 @@
+REM git clone https://github.com/alcenirfelix/Q-Life.git
+REM -----------------------------------------------------------------
+REM git remote add origin https://github.com/alcenirfelix/Q-Life.git
+REM git push -u origin master
+git pull
+git status
+git fecth
