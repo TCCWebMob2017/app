72d83e878a6213f30497bfb4784fa7d45a19ba89
alcenirfelix <afelix@softquim.com.br>
2019-02-24 20:12:18 -0300

    Commit

--- _backup/src_1/app/models/doencas.ts
+++
@@ -1,7 +0,0 @@
-export class Doencas {
-    id: string;
-    nome: string;
-    descricao: string;
-    nomesPopulares: string;
-    cid: string;
-}
--- _backup/src_2 jwt/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  //serverURL: 'http://localhost:8080',
-  serverURL: 'https://api-qlife.herokuapp.com',
-  //whitelistedDomains: ['localhost:8080']
-  whitelistedDomains: ['https://api-qlife.herokuapp.com']
-};
