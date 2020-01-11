c7620b04cb25addc9922e5f327cfe38b9a87f5cc
Letícia Meirelles <leticia.meirelles@cyan-agro.com>
2020-01-11 17:34:48 -0300

    bump libs

--- src/app/pessoal-medicamentos-add/pessoal-medicamentos-add.page.ts
+++ src/app/pessoal-medicamentos-add/pessoal-medicamentos-add.page.ts
@@ -1,40 +1,41 @@
-import { PessoalService } from './../services/pessoal.service';
-import { NavController, AlertController, Platform } from '@ionic/angular';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { StorageService } from '../services/storage.service';
-import { ActivatedRoute, NavigationExtras } from '@angular/router';
+import { PessoalService } from "./../services/pessoal.service";
+import { NavController, AlertController, Platform } from "@ionic/angular";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { StorageService } from "../services/storage.service";
+import { ActivatedRoute, NavigationExtras } from "@angular/router";
 
 @Component({
-  selector: 'app-pessoal-medicamentos-add',
-  templateUrl: './pessoal-medicamentos-add.page.html',
-  styleUrls: ['./pessoal-medicamentos-add.page.scss'],
+  selector: "app-pessoal-medicamentos-add",
+  templateUrl: "./pessoal-medicamentos-add.page.html",
+  styleUrls: ["./pessoal-medicamentos-add.page.scss"]
 })
 export class PessoalMedicamentosAddPage implements OnInit {
-  @ViewChild('searchBar') myInput ;
+  @ViewChild("searchBar", { static: false }) myInput;
 
-  public  lista_items       : any;
-          searchTerm        : string = '';
+  public lista_items: any;
+  searchTerm: string = "";
 
-  constructor(public  navCtrl         : NavController, 
-              public  pessoalService  : PessoalService,
-              private storage         : StorageService,
-              private activatedRoute  : ActivatedRoute,
-              public  alertController : AlertController) { }
+  constructor(
+    public navCtrl: NavController,
+    public pessoalService: PessoalService,
+    private storage: StorageService,
+    private activatedRoute: ActivatedRoute,
+    public alertController: AlertController
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   ionViewWillEnter() {
     setTimeout(() => {
       this.myInput.setFocus();
-    }, 150);     
+    }, 150);
   }
 
-  ionViewDidLoad(){}
-  ionViewDidEnter(){}
-  ionViewWillLeave(){}
-  ionViewDidLeave(){}
-  ionViewWillUnload(){}
+  ionViewDidLoad() {}
+  ionViewDidEnter() {}
+  ionViewWillLeave() {}
+  ionViewDidLeave() {}
+  ionViewWillUnload() {}
 
   /*
   setFilteredItems(ev: any) {
@@ -42,34 +43,52 @@ export class PessoalMedicamentosAddPage implements OnInit {
     this.medicamentos = this.pessoalService.filterItems(this.medicamentos_all, val);
   }
   */
- 
+
   selecionarRegistro(value: any) {
-    if (value!= null) { 
+    if (value != null) {
       this.alertConfirmarAdicaoDeItem(value);
     }
   }
 
-  async alertConfirmarAdicaoDeItem(value : any) {
+  async alertConfirmarAdicaoDeItem(value: any) {
     const alert = await this.alertController.create({
-      header: 'Adicionar medicamento',
-      message: 'O item <b>' + value['nome'] + '</b> será adicionado.',
+      header: "Adicionar medicamento",
+      message: "O item <b>" + value["nome"] + "</b> será adicionado.",
       inputs: [
-        { name: 'frequencia',       type: 'text', value: '', placeholder: 'Frequência de uso' },
-        { name: 'dosagem',          type: 'text', value: '', placeholder: 'Dosagem' },
-        { name: 'viaAdministracao', type: 'text', value: '', placeholder: 'Via de administração' },
-        { name: 'observacao',       type: 'text', value: '', placeholder: 'Observação' }
+        {
+          name: "frequencia",
+          type: "text",
+          value: "",
+          placeholder: "Frequência de uso"
+        },
+        { name: "dosagem", type: "text", value: "", placeholder: "Dosagem" },
+        {
+          name: "viaAdministracao",
+          type: "text",
+          value: "",
+          placeholder: "Via de administração"
+        },
+        {
+          name: "observacao",
+          type: "text",
+          value: "",
+          placeholder: "Observação"
+        }
       ],
       buttons: [
         {
-          text: 'Cancel', role: 'cancel', cssClass: 'secondary',
+          text: "Cancel",
+          role: "cancel",
+          cssClass: "secondary",
           handler: () => {
             //console.log('Confirm Cancel');
           }
-        }, {
-          text: 'Ok',
-          handler: ( data = Response ) => {
+        },
+        {
+          text: "Ok",
+          handler: (data = Response) => {
             let _value = this.addRegistro(value, data);
-            this.navCtrl.navigateBack(['pessoal-medicamentos']);
+            this.navCtrl.navigateBack(["pessoal-medicamentos"]);
           }
         }
       ]
@@ -77,33 +96,32 @@ export class PessoalMedicamentosAddPage implements OnInit {
     await alert.present();
   }
 
-  addRegistro(value : any, data : any) : any {
+  addRegistro(value: any, data: any): any {
     let _novoRegistro = value;
-    let _medicamento = { };
-    _medicamento['privacidade']       = { };
-    _medicamento['frequencia']        = data['frequencia'];
-    _medicamento['dosagem']           = data['dosagem'];
-    _medicamento['viaAdministracao']  = data['viaAdministracao'];
-    _medicamento['observacao']        = data['observacao'];
-    _medicamento['medicamento']       = _novoRegistro;
+    let _medicamento = {};
+    _medicamento["privacidade"] = {};
+    _medicamento["frequencia"] = data["frequencia"];
+    _medicamento["dosagem"] = data["dosagem"];
+    _medicamento["viaAdministracao"] = data["viaAdministracao"];
+    _medicamento["observacao"] = data["observacao"];
+    _medicamento["medicamento"] = _novoRegistro;
     this.storage.addMedicamentos(_medicamento);
     return _medicamento;
-  }    
-
-  search(nome : string) {
-    nome = nome.toLowerCase();
-    this.pessoalService.getMedicamentosPorNome(nome)
-    .subscribe(Response => {
-      this.lista_items = Response;
-
-      setTimeout(() => {
-        this.myInput.setFocus();
-      }, 150);
-
-    },
-    error => {
-      //console.log(error);
-    });
   }
 
+  search(nome: string) {
+    nome = nome.toLowerCase();
+    this.pessoalService.getMedicamentosPorNome(nome).subscribe(
+      Response => {
+        this.lista_items = Response;
+
+        setTimeout(() => {
+          this.myInput.setFocus();
+        }, 150);
+      },
+      error => {
+        //console.log(error);
+      }
+    );
+  }
 }
--- src/app/sobre/sobre.page.ts
+++ src/app/sobre/sobre.page.ts
@@ -1,67 +1,58 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { IonItemSliding } from '@ionic/angular';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { IonItemSliding } from "@ionic/angular";
 
 @Component({
-  selector: 'app-sobre',
-  templateUrl: './sobre.page.html',
-  styleUrls: ['./sobre.page.scss'],
+  selector: "app-sobre",
+  templateUrl: "./sobre.page.html",
+  styleUrls: ["./sobre.page.scss"]
 })
 export class SobrePage implements OnInit {
-
   segment: number;
-  @ViewChild("slides") slides;
+  @ViewChild("slides", { static: false }) slides;
 
-  public list: string[] = ['A', 'B', 'C', 'D', 'E']; //, 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'];
+  public list: string[] = ["A", "B", "C", "D", "E"]; //, 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O'];
 
-  constructor() { 
+  constructor() {
     this.segment = 0;
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   public async setSegment(activeIndex: Promise<number>) {
     this.segment = await activeIndex;
   }
-    
-
-
 
   deletarItem() {
-    console.log('Delete');
+    console.log("Delete");
   }
 
   async modificarItem(slidingItem: IonItemSliding) {
     await slidingItem.close();
-    console.log('Modificar');
+    console.log("Modificar");
   }
-
 
   async deleteItem(slidingItem: IonItemSliding, item: any) {
     slidingItem.close();
     //await this.itemService.deleteItem(item);
-    console.log('deleteItem');
+    console.log("deleteItem");
   }
 
   ionSwipeItem(par1, par2) {
-    console.log('IonSwipeItem');
+    console.log("IonSwipeItem");
     console.log(par1);
     console.log(par2);
   }
 
-  async del(slidingItem: IonItemSliding, event, item: any, index: number){
-
+  async del(slidingItem: IonItemSliding, event, item: any, index: number) {
     await slidingItem.close();
     let indexx = this.list.indexOf(item);
-    
-    console.log('Item: ' + item + ' / Index: ' + index);
+
+    console.log("Item: " + item + " / Index: " + index);
     console.log(slidingItem);
     console.log(event);
 
-    if(index > -1){
-        this.list.splice(index, 1);
+    if (index > -1) {
+      this.list.splice(index, 1);
     }
   }
-
-
 }
